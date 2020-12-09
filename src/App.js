bab5665620349bee73bca7fdac08d8fba66ed62f
Brian Reisman <brianreisman@protonmail.com>
2020-12-09 15:28:20 -0500

    empty components set up and axios call working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+import dummyData from './data';
+import Description from './components/Description'
+import Title from './components/Title'
+import Visual from './components/Visual'
 
 function App() {
+  const [data, setData] = useState(dummyData)
+
+  
+useEffect(()=>{
+  // axios
+  //   .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  //   .then(res => {
+  //     console.log(res)
+  //     console.log(res.data)
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //     throw new Error('OOOOOPPSE!')
+  //   })
+})
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <Title/>
+        <Visual/>
+        <Description/>
       </p>
     </div>
   );
---
+++ src/components/Visual.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Visual = () => {
+ return(
+  <>
+    <div>Visual</div>
+  </>
+
+ ); 
+}
+
+export default Visual;
